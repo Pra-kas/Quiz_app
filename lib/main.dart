26cb260129bc6f693ba78e2908c6480fbf311398
Pra_Kas <prakash63690@gmail.com>
2023-12-28 23:03:39 +0530

    practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 void main() {
-<<<<<<< HEAD
   runApp(const MyApp());
 }
 
@@ -40,7 +39,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
+  // This widget isthe home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
 
@@ -121,71 +120,6 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
-=======
-  runApp(const Trial());
-}
-
-class Trial extends StatefulWidget {
-  const Trial({Key? key}) : super(key: key);
-
-  @override
-  State<Trial> createState() => _TrialState();
-}
-
-class _TrialState extends State<Trial> {
-  var _volume = 10.0;
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.pink,
-        ),
-        endDrawer: Drawer(
-          backgroundColor: const Color.fromARGB(255, 146, 182, 248),
-          child: Column(
-            children: <Widget>[
-              const UserAccountsDrawerHeader(
-                accountName: Text("Prakash"),
-                accountEmail: Text("prakash63690@gmail.com"),
-                currentAccountPicture: CircleAvatar(),
-              ),
-              const ListTile(
-                title: Text("Prakash"),
-                leading: Icon(Icons.mail),
-              ),
-              const Divider(
-                endIndent: 0.5,
-              ),
-              ListTile(
-                title: const Text("Prakash"),
-                leading: IconButton(
-                  onPressed: () {
-                    
-                  },
-                  icon: const Icon(Icons.mail),
-                ),
-              ),
-              const Divider(
-                endIndent: 0.5,
-              ),
-            ],
-          ),
-        ),
-        body:  Center(
-          child : Ink(
-            decoration: const ShapeDecoration(
-              color: Colors.blueAccent,
-              shape: CircleBorder(
-              ),
-            ),
-             child : IconButton(
-                icon : const Icon(Icons.add), onPressed: () {  },
-              )
-          )
-      ),
-      ),
->>>>>>> prakash-kumar_dev
     );
   }
 }
